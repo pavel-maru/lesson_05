09cfa5eff4d5315e156953a050a880b9b1ff018e
pavel-maru <71653912+pavel-maru@users.noreply.github.com>
2021-02-08 13:27:42 +0300

    Update calc_for_any_range.py

--- calc_for_any_range.py
+++ calc_for_any_range.py
@@ -1,72 +1,76 @@
 
-# Программа сложения двух чисел в любой системе счисления. 
+# Программа сложения двух чисел в любой системе счисления.
 
 from collections import deque
 
-print("Калькулятор позволяет суммировать числа в любой системе счисления.")
-BASE = int(input('Введите размерность (число от 2 до 36): '))
-# DIGITS = ('0', '1', '2')
+print("\nКалькулятор позволяет суммировать числа в системах счисления от двоичной  до 36-ричной.\n")
+BASE = int(input('Введите размерность системы счисления (от 2 до 36): '))
+# DIGITS = ('0', '1', '2', '3', '4', '5','6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F')
 
-FIRST_LETTER = 97
-
-if BASE <= 10:
-    DIGITS = [str(sym) for sym in range(BASE)]
-    DIGITS = tuple(DIGITS)
-    # print(type(DIGITS), DIGITS)
+# формируем набор символов (цифр)
+DEC = 10
+if BASE <= DEC:
+    DIGITS = [str(i) for i in range(BASE)]
 else:
-    DIGITS = [str(sym) for sym in range(10)]
+    DIGITS = [str(i) for i in range(DEC)]
     # print(type(DIGITS))
-    for i in range(FIRST_LETTER, FIRST_LETTER + BASE - 10):
-        DIGITS.append(chr(i).upper())
-    DIGITS = tuple(DIGITS)
-    # print(type(DIGITS), DIGITS)
+    FIRST_LETTER = ord('A')
+    # print(FIRST_LETTER)
+    # for i in range(FIRST_LETTER, FIRST_LETTER + BASE - 10):
+    #     DIGITS.append(chr(i).upper())
+    DIGITS.extend(chr(i) for i in range(FIRST_LETTER, FIRST_LETTER + BASE - DEC))
 
-print(f'\nПоследовательность символов (цифр) в данной размерности:\n{DIGITS}\n')
+DIGITS = tuple(DIGITS)
+# print(type(DIGITS), DIGITS)
+
+print(f'\nПоследовательность символов (цифр) в данной размерности:\n', ', '.join(DIGITS))
 
 num_res = deque()
 # print(type(num_res))
 overflow = 0
 
-num_1  = deque(input('Введите первое число в выбранном представлении: ').upper())
-num_2  = deque(input('Введите второе число в выбранном представлении: ').upper())
+num_1  = deque(input('\nВведите первое число в выбранном представлении: ').upper())
+num_2  = deque(input('\nВведите второе число в выбранном представлении: ').upper())
 # print(number_1, number_2)
 
 # print(digits.index(num_1[len(num_1) - 1]))
 
 len_res = max(len(num_1), len(num_2)) + 1
-
+# вычисление суммы
 for i in range(len_res):
-    if len(num_1) - i > 0:
+    # добываем последовательно цифры из введённых чисел
+    if i < len(num_1):
         dig_1 = num_1[len(num_1) - 1 - i]
     else:
         dig_1 = '0'
-    if len(num_2) - i > 0:
+    if i < len(num_2):
         dig_2 = num_2[len(num_2) - 1 - i]
     else:
         dig_2 = '0'
     # print(dig_1, dig_2)
-
-    index_spam = DIGITS.index(dig_1) + DIGITS.index(dig_2) + overflow
-
-    if index_spam < BASE:
-        num_res.appendleft(DIGITS[index_spam])
+    # определяем результирующую цифру по индексам
+    index_ = DIGITS.index(dig_1) + DIGITS.index(dig_2) + overflow
+    if index_ < BASE:
+        num_res.appendleft(DIGITS[index_])
         # print(num_res)
         overflow = 0
     else:
-        num_res.appendleft(DIGITS[index_spam - BASE])
+        num_res.appendleft(DIGITS[index_ - BASE])
         # print(num_res)
         overflow = 1
 
-# print(num_res)
-
 # num_res.reverse()
 # print(num_res)
-
+# удаляем незначащий ноль в начале числа
 if num_res[0] == '0':
     del num_res[0]
 # print(num_res)
 
-num_1 = ''.join(num_1)
-num_2 = ''.join(num_2)
-num_res = ''.join(num_res)
-print(f'{num_1} + {num_2} = {num_res}')
+# del num_res[0] if num_res[0] == '0'
+
+# преобразуем массивы чисел в строчный вид и выводим
+# num_1 = ''.join(num_1)
+# num_2 = ''.join(num_2)
+# num_res = ''.join(num_res)
+# print(f'{num_1} + {num_2} = {num_res}')
+print('\n', ''.join(num_1), '+', ''.join(num_2), '=', ''.join(num_res))
